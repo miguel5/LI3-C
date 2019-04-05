2756d0a5befa0cd76e23a8061e42ab2b0132ab2c
barbospl <48600875+barbospl@users.noreply.github.com>
2019-04-05 22:07:41 +0100

    alteração nos menus

--- navegacao.c
+++ navegacao.c
@@ -6,13 +6,15 @@ int um_first_question()
 {
     char escolha;
     printf("Pretende utilizar ficheiros altenativos? s ou n\n");
-    scanf("%s", escolha);
-    while(escolha != "s" || escolha != "n")
+    scanf("%c", &escolha);
+    getchar();
+    while(escolha != 's' && escolha != 'n')
     {
         printf("Escolha inválida. Responda com s ou n.\n");
-        scanf("%s", escolha);
+        scanf("%c", &escolha);
+        getchar();
     }
-    if (escolha == "s")
+    if (escolha == 's')
         return 1;
     else 
         return 0;
@@ -38,7 +40,7 @@ void um_present_result(char* prod_filename, char* cli_filename, char* venda_file
 
 void main_menu()
 {
-    system("cls");
+    system("clear");
     printf("Introduza a opção pretendida\n");
     printf("1 - Ler ficheiros\n");
     printf("2 - Lista de produtos por letra\n");
@@ -52,13 +54,12 @@ void main_menu()
     printf("10 - Lista dos mais comprados por cliente\n");
     printf("11 - Lista dos produtos mais vendidos\n");
     printf("12 - 3 maiores gastos por cliente\n");
-    printf("q - Terminar o programa\n");
+    printf("0 - Terminar o programa\n");
 }
 
 void opcao_invalida()
 {
-    system("cls");
-    printf("Opção inválida.\nIntroduza número de 1 a 12 ou q para terminar.\n");
-    sleep(5);
-    system("cls");
+    system("clear");
+    printf("Opção inválida.\nDeve introduzir número de 1 a 12 ou 0 para terminar.\n");
+    sleep(2);
 }
