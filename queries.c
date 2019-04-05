a212c573a9aabd4cd5fdd9af050fce7ddf27457c
barbospl <48600875+barbospl@users.noreply.github.com>
2019-04-05 22:09:05 +0100

    Alterada query 1; não finalizada

--- queries.c
+++ queries.c
@@ -5,41 +5,29 @@
 #include <gmodule.h>
 
 /* Recebe apontador para sistema de gestão de vendas da main */
-void query_um(CatProd prod_list, CatCli cli_list) /* Vai ser alterado devido a dependências */
+void query_um(CatProd prod_list, CatCli cli_list, char* prod_filename, char* cli_filename, char* venda_filename) /* Vai ser alterado devido a dependências */
 {    
-    int prod_validos, cli_validos, vendas_validos;
-    char* prod_filename = NULL;
-    char* cli_filename = NULL;
-    char* venda_filename = NULL;
-
     /* Funções para determinar se o sistema atual contém dados. Se existirem, têm de ser apagados */
-    if (!isProdEmpty(prod_list))
-        freeProd(prod_list);
-    if (!isCliEmpty(cli_list))
-        freeCli(cli_list);
+    freeProd(prod_list);
+    freeCli(cli_list);
     /*
-    if (!isFatEmpty(faturacao))
-        freeFaturacao(faturacao);
+    freeFaturacao(faturacao);
     for (int i = 0; i < 3; i++)    
-        if (!isFilEmpty(filial[i]))
             freeFilial(filial[i]);
     */
 
     prod_list = newCatProd();
-    cli_list = newCliente();
+    cli_list = newCatCli();
 
     /* Receber do utilizador a proveniência da informação: nome dos ficheiros ou ficheiros por omissão */
     if (um_first_question()) /* Retorna 1 ou 0 */        
         um_second_question(prod_filename, cli_filename, venda_filename);
 
-    prod_validos = lerProdutos(prod_list, prod_filename);
-    cli_validos = lerClientes(cli_list, cli_filename);
     /*
     fillFaturacao(full_data -> faturacao);
     for (i = 0; i < 3; i++)
         fillFilial(full_data -> filial[3]);
     */
-    um_present_result(prod_filename, cli_filename, venda_filename, prod_validos, cli_validos, vendas_validos);
 }
 
 void query_tres(Facturacao f, int mes, char* prod, int glob)
