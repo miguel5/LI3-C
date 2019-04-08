45acdcced3a258e3ac519a2df0adfd538e6faace
barbospl <48600875+barbospl@users.noreply.github.com>
2019-04-08 16:14:44 +0100

    include gmodule

--- navegacao.h
+++ navegacao.h
@@ -1,10 +1,12 @@
 #ifndef NAVEGACAO_H
 #define NAVEGACAO_H
 
+#include <gmodule.h>
+
 int um_first_question();
 void um_second_question(char* prod_filename, char* cli_filename, char* venda_filename);
 void um_present_result(char* prod_filename, char* cli_filename, char* venda_filename, int prod_validos, int cli_validos, int vendas_validos);
-
+void dois_pergunta(char*);
 void tres_question(int* mes, char ** produto, int* glob);
 void tres_present_result(char* result);
 
